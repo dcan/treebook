ab8cd5c42605481465d97baf5c7559843a87ba9e
Diyar Can <diyar.can1@gmail.com>
2013-12-14 12:05:26 +0100

    Add status validations and tests

--- test/functional/statuses_controller_test.rb
+++ test/functional/statuses_controller_test.rb
@@ -17,7 +17,7 @@ class StatusesControllerTest < ActionController::TestCase
     assert_redirected_to new_user_session_path
   end
 
-  test "should be redirected when not logged in" do
+  test "should be redirected when not logged in 2" do
     sign_in users(:diyar)
     get :new
     assert_response :success
@@ -29,7 +29,10 @@ class StatusesControllerTest < ActionController::TestCase
     assert_redirected_to new_user_session_path
   end
 
-  test "should create status" do
+  test "should create status when logged in" do
+    sign_in users(:diyar)
+
+
     assert_difference('Status.count') do
       post :create, status: { content: @status.content }
     end
@@ -37,17 +40,32 @@ class StatusesControllerTest < ActionController::TestCase
     assert_redirected_to status_path(assigns(:status))
   end
 
-  test "should show status" do
+
+  test "should show status when logged in" do
+    sign_in users(:diyar)
+
     get :show, id: @status
     assert_response :success
   end
 
-  test "should get edit" do
+
+  test "should get edit when logged in" do
+    sign_in users(:diyar)
+
     get :edit, id: @status
     assert_response :success
   end
 
-  test "should update status" do
+
+  test "should redirect status update when not logged in" do
+    put :update, id: @status, status: { content: @status.content }
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should update status when logged in" do
+    sign_in users(:diyar)
+
     put :update, id: @status, status: { content: @status.content }
     assert_redirected_to status_path(assigns(:status))
   end
